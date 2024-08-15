274a6d1e39a091efea727a0e072f6631e97602cb
Srijan Sahay <srijan05sahay@gmail.com>
2024-08-15 16:13:29 +0530

    basic polling function added

--- Polls/admin.py
+++ Polls/admin.py
@@ -1,5 +1,20 @@
 from django.contrib import admin
 from .models import Question, Choice
 # Register your models here.
-admin.site.register(Question)
+# admin.site.register(Question)
 admin.site.register(Choice)
+
+class ChoiceInLine(admin.StackedInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {"fields":["questionText"]}),
+        ("Date Information", {"fields": ["pubDate"], "classes":["collapse"]})
+    ]
+    inlines = [ChoiceInLine]
+
+
+
+admin.site.register(Question, QuestionAdmin)
